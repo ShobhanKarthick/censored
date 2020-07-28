5c0ebbf10e4c483904f4fa7203a9fcd9f1357377
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-07-28 21:39:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import './Components/Styles.css';
+import Home from './Components/Home';
+import Play from './Components/Play';
+import HowToPlay from './Components/HowToPlay';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/play" exact component={Play} />
+      <Route path="/howtoplay" exact component={HowToPlay} />
+      <Route path="/about" exact component={About} />
+    </Router>
   );
 }
 
---
+++ src/Components/HowToPlay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HowToPlay() {
+    return (
+        <div>
+            <h1>This is How to Play page</h1>
+        </div>
+    )
+}
+
+export default HowToPlay
