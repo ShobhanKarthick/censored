213035affcd81ed7386f41ed5ab2423d2ade9c57
ShobhanKarthick <63546843+ShobhanKarthick@users.noreply.github.com>
2020-08-19 16:06:08 +0530

    adding more categories

--- client/src/Components/Upload.js
+++ client/src/Components/Upload.js
@@ -48,13 +48,22 @@ function Upload() {
       });
 
     document.getElementById("Scientist").checked = false
+    document.getElementById("Inventor").checked = false
     document.getElementById("Entrepreneur").checked = false
-    document.getElementById("Sportsman").checked = false
+    document.getElementById("Economist").checked = false
+    document.getElementById("Footballer").checked = false
+    document.getElementById("Cricketer").checked = false
+    document.getElementById("Basketballer").checked = false
+    document.getElementById("ChessPlayer ").checked = false
     document.getElementById("Celebrity").checked = false
     document.getElementById("Humanitarian").checked = false
-    document.getElementById("Inventor").checked = false
     document.getElementById("Authors").checked = false
-    document.getElementById("Historical ").checked = false
+    document.getElementById("HistoricalFigure").checked = false
+    document.getElementById("Magician").checked = false
+    document.getElementById("Youtuber").checked = false
+    document.getElementById("Singer").checked = false
+    document.getElementById("Rapper").checked = false
+    document.getElementById("UnderworldDon ").checked = false
     
     setClue([]);
     setAnswer("");
@@ -68,14 +77,23 @@ function Upload() {
       <fieldset className="upload-tags-container">
         <legend><p>Tags</p></legend>
         <div><input id="Scientist" className='upload-tag-input' type='checkbox' value="Scientist" onChange={clueHandler} /><label className="upload-input-label" for="Scientist">Scientist</label></div>
+        <div><input id="Inventor" className='upload-tag-input' type='checkbox' value="Inventor" onChange={clueHandler} /><label className="upload-input-label" for="Inventor">Inventor</label></div>
         <div><input id="Entrepreneur" className='upload-tag-input' type='checkbox' value="Entrepreneur" onChange={clueHandler} /><label className="upload-input-label" for="Entrepreneur">Entrepreneur</label></div>
-        <div><input id="Sportsman" className='upload-tag-input' type='checkbox' value="Sportsman" onChange={clueHandler} /><label className="upload-input-label" for="Sportsman">Sportsman</label></div>
+        <div><input id="Economist" className='upload-tag-input' type='checkbox' value="Basketballer" onChange={clueHandler} /><label className="upload-input-label" for="Basketballer">Basketballer</label></div>
+        <div><input id="Footballer" className='upload-tag-input' type='checkbox' value="Footballer" onChange={clueHandler} /><label className="upload-input-label" for="Footballer">Footballer</label></div>
+        <div><input id="Cricketer" className='upload-tag-input' type='checkbox' value="Cricketer" onChange={clueHandler} /><label className="upload-input-label" for="Cricketer">Cricketer</label></div>
+        <div><input id="Basketballer" className='upload-tag-input' type='checkbox' value="Economist" onChange={clueHandler} /><label className="upload-input-label" for="Economist">Economist</label></div>
+        <div><input id="ChessPlayer" className='upload-tag-input' type='checkbox' value="Chess Player" onChange={clueHandler} /><label className="upload-input-label" for="Chess Player">Chess Player</label></div>
         <div><input id="Celebrity" className='upload-tag-input' type='checkbox' value="Celebrity" onChange={clueHandler} /><label className="upload-input-label" for="Celebrity">Celebrity</label></div>
         <div><input id="Humanitarian" className='upload-tag-input' type='checkbox' value="Humanitarian" onChange={clueHandler} /><label className="upload-input-label" for="Humanitarian">Humanitarian</label></div>
-        <div><input id="Inventor" className='upload-tag-input' type='checkbox' value="Inventor" onChange={clueHandler} /><label className="upload-input-label" for="Inventor">Inventor</label></div>
         <div><input id="Authors" className='upload-tag-input' type='checkbox' value="Authors" onChange={clueHandler} /><label className="upload-input-label" for="Authors">Authors</label></div>
-        <div><input id="Historical Figure" className='upload-tag-input' type='checkbox' value="Historical Figure" onChange={clueHandler} /><label className="upload-input-label" for="Historical Figure">Historical Figure</label></div>
-      </fieldset>
+        <div><input id="HistoricalFigure" className='upload-tag-input' type='checkbox' value="Historical Figure" onChange={clueHandler} /><label className="upload-input-label" for="Historical Figure">Historical Figure</label></div>
+        <div><input id="Magician" className='upload-tag-input' type='checkbox' value="Magician" onChange={clueHandler} /><label className="upload-input-label" for="Magician">Magician</label></div>
+        <div><input id="Youtuber" className='upload-tag-input' type='checkbox' value="Youtuber" onChange={clueHandler} /><label className="upload-input-label" for="Youtuber">Youtuber</label></div>
+        <div><input id="Singer" className='upload-tag-input' type='checkbox' value="Singer" onChange={clueHandler} /><label className="upload-input-label" for="Singer">Singer</label></div>
+        <div><input id="Rapper" className='upload-tag-input' type='checkbox' value="Rapper" onChange={clueHandler} /><label className="upload-input-label" for="Rapper">Rapper</label></div>
+        <div><input id="UnderworldDon" className='upload-tag-input' type='checkbox' value="Underworld Don" onChange={clueHandler} /><label className="upload-input-label" for="Underworld Don">Underworld Don</label></div>
+        </fieldset>
       <input
           style={{ marginBottom: "20px" }}
           id='answer-input'
